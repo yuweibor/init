519f55a2e2441df45dd6231123e2209c04464e1a
weibor <yuweibor@me.com>
2023-06-01 16:06:46 +0800

    redux

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { bindActionCreators, combineReducers, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 
 export async function loader({ request }) {
     return null;
@@ -24,15 +25,19 @@ const reducer2 = (state = initState, action) => {
         case 'a2':
             return { ...state, ...action.payload }
             break;
+        case 'a3':
+            return { ...state, ...action.payload }
+            break;
+        case 'a4':
+            return { ...state, ...action.payload }
+            break;
         default:
             return state
     }
 }
-
-
-// const actions = bindActionCreators(action1, action2)
 const reducers = combineReducers({ reducer1, reducer2 });
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+
+const store = createStore(reducers, applyMiddleware(thunk, promise, logger));
 
 const act1 = (payload) => {
     return {
@@ -46,24 +51,38 @@ const act2 = (payload) => {
         payload
     }
 }
-// store.dispatch(act1({ name: 'a1' }));
 
-const act = { act1, act2 }
-const newAct = bindActionCreators(act, store.dispatch);
-newAct.act1({ name: 'a11' })
-
-
-
-const act3 = () => {
+// thunk 需要用到dispatch
+const act3 = (payload) => {
     return async (dispatch) => {
-        await new Promise(e => setTimeout(e, 1000));
-        dispatch({ type: 'a2', payload: { name: 'a2' } })
+        const res = await new Promise(e => setTimeout(() => e('thunk'), 1000));
+        dispatch({ type: 'a3', payload: { name: payload.name + res } })
     }
 }
-store.dispatch(act3() as any);
 
+// promise 只需返回promise
+const act4 = (payload) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res({ type: "a4", payload: { name: payload.name + 'promise' } })
+        }, 1000)
+    })
+}
 
+// 第一种
+store.dispatch(act1({ name: 'a11' }));
 
+// 第二种 整合action
+const act = { act1, act2, act3, act4 }
+const newAct = bindActionCreators(act, store.dispatch);
+newAct.act2({ name: 'a22' })
+
+// 第三种 redux-thunk
+newAct.act3({ name: 'a33' })
+// 第四种 redux-promise
+newAct.act4({ name: 'a44' })
+
+// 第四种 redux-promise
 
 export function Component() {
     // let data = useLoaderData();
